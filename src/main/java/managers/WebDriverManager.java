66b28095f9ae68bd8ba761b32715d7d779909615
Jenkins <jenkins@pc-00203.bg.westum.com>
2018-12-21 14:42:17 +0200

    remoteWrapper

--- src/main/java/managers/WebDriverManager.java
+++ src/main/java/managers/WebDriverManager.java
@@ -1,5 +1,7 @@
 package managers;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Dimension;
@@ -9,6 +11,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import enums.DriverType;
 import enums.EnvironmentType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class WebDriverManager {
     private WebDriver driver;
@@ -21,12 +25,12 @@ public class WebDriverManager {
         environmentType = FileReaderManager.getInstance().getConfigReader().getEnvironment();
     }
 
-    public WebDriver getDriver() {
+    public WebDriver getDriver() throws MalformedURLException, InterruptedException {
         if(driver == null) driver = createDriver();
         return driver;
     }
 
-    private WebDriver createDriver() {
+    private WebDriver createDriver() throws MalformedURLException, InterruptedException {
         switch (environmentType) {
             case LOCAL : driver = createLocalDriver();
                 break;
@@ -36,8 +40,25 @@ public class WebDriverManager {
         return driver;
     }
 
-    private WebDriver createRemoteDriver() {
-        throw new RuntimeException("RemoteWebDriver is not yet implemented");
+    private WebDriver createRemoteDriver() throws MalformedURLException, InterruptedException {
+        URL server = new URL("http://0.0.0.0:4444/wd/hub");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+
+        System.out.println("Connecting to " + server);
+
+        WebDriver driver = new RemoteWebDriver(server, capabilities);
+
+        driver.get("https://www.blu.com/en/GB");
+
+        Thread.sleep(2000);
+        ((RemoteWebDriver) driver).findElementByXPath("//button[contains(text(), 'Yes, I am over 18')]").click();
+        Thread.sleep(3000);
+
+        driver.quit();
+
+        return driver;
     }
 
     private WebDriver createLocalDriver() {
